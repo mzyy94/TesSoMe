c1507b125942e43a3ce98ef40b4b9088fb4db21b
Yuki MIZUNO <mizuyuu0904@gmail.com>
2014-09-13 05:25:13 +0900

    Create TessoApiManager's Tests

--- TesSoMeTests/TesSoMeTests.swift
+++ TesSoMeTests/TesSoMeTests.swift
@@ -10,14 +10,30 @@ import UIKit
 import XCTest
 
 class TesSoMeTests: XCTestCase {
+    let userId = "YOUR_USERID_HERE"
+    let password = "YOUR_PASSWORD_HERE"
+    
+    let apiMgr = TessoApiManager()
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let apiExpectation = self.expectationWithDescription("api signin")
+        
+        apiMgr.signIn(self.userId, password: self.password, onSuccess: {
+            XCTAssert(true, "Pass")
+            apiExpectation.fulfill()
+            }, onFailure: { err in
+                XCTAssert(false, "Fail")
+        })
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: {err in
+        })
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        apiMgr.signOut(nil, onFailure: nil)
         super.tearDown()
     }
     
@@ -26,10 +42,26 @@ class TesSoMeTests: XCTestCase {
         XCTAssert(true, "Pass")
     }
     
+    func testGetTimeline() {
+        let apiExpectation = self.expectationWithDescription("api signin")
+        
+        apiMgr.getData(1, topicid: 1, maxid: nil, sinceid: nil, tag: nil, username: nil, type: nil, onSuccess:
+            {res, data in
+                XCTAssert(true, "Pass")
+                apiExpectation.fulfill()
+            }
+            , onFailure: {res, err in
+                XCTAssert(false, "Fail")
+        })
+        self.waitForExpectationsWithTimeout(3.0, handler: {err in
+        })
+    }
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock() {
             // Put the code you want to measure the time of here.
+            //XCTAssertEqual("a", "b", "err")
         }
     }
     
