4b28994523157b5daf870ce0c3aa7a2ca280bfb2
Yuki MIZUNO <mizuyuu0904@gmail.com>
2014-09-13 02:51:25 +0900

    Install CocoaPods and add AFNetworking

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, "8.0"
+
+target "TesSoMe" do
+pod 'AFNetworking'
+
+end
+
+target "TesSoMeTests" do
+
+end
+
---
+++ TesSoMe/Pods-ObjC-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Pods-ObjC-Bridging-Header.h
+//  TesSoMe
+//
+//  Created by Yuki Mizuno on 2014/09/13.
+//  Copyright (c) 2014年 Yuki Mizuno. All rights reserved.
+//
+
+#ifndef TesSoMe_Pods_ObjC_Bridging_Header_h
+#define TesSoMe_Pods_ObjC_Bridging_Header_h
+
+#import <AFNetworking/AFNetworking.h>
+
+
+#endif
