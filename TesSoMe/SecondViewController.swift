ec06e4dd36f031476b53ceb2929e583553e9a14a
Yuki MIZUNO <mizuyuu0904@gmail.com>
2014-09-12 19:46:57 +0900

    Initial Commit

---
+++ TesSoMe/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TesSoMe
+//
+//  Created by Yuki Mizuno on 2014/09/12.
+//  Copyright (c) 2014年 Yuki Mizuno. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
