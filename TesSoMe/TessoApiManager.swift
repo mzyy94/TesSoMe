aec32c627b4af8d4e3fcad14ec23001fcb32f9bd
Yuki MIZUNO <mizuyuu0904@gmail.com>
2014-09-16 18:18:16 +0900

    Add two enums named TesSoMeGetMode and TesSoMeSearchType

--- TesSoMe/TessoApiManager.swift
+++ TesSoMe/TessoApiManager.swift
@@ -11,6 +11,17 @@ import UIKit
 class TessoApiManager: NSObject {
 	let apiEndPoint = "https://tesso.pw/sns/api"
 	
+	enum TesSoMeGetMode: Int {
+		case Test = 0, Timeline, Topic, Class, Profile, SearchResult;
+	};
+	
+	enum TesSoMeSearchType: Int {
+		case All = -1
+		case Message = 0
+		case Drawing = 1
+		case File = 2
+	};
+	
 	func signIn(#userId: String, password: String, onSuccess: (() -> Void)! = nil, onFailure: ((NSError) -> Void)! = nil) {
 		var sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
 		sessionConfig.HTTPShouldSetCookies = true
@@ -59,13 +70,13 @@ class TessoApiManager: NSObject {
 		}
 	}
 	
-	func getData(#mode: Int, topicid: Int? = nil, maxid: Int? = nil, sinceid: Int? = nil, tag: String? = nil, username: String? = nil, type: Int? = nil, onSuccess: ((NSDictionary) -> Void)! = nil, onFailure: ((NSError) -> Void)! = nil) {
+	func getData(#mode: TesSoMeGetMode, topicid: Int? = nil, maxid: Int? = nil, sinceid: Int? = nil, tag: String? = nil, username: String? = nil, type: Int? = nil, onSuccess: ((NSDictionary) -> Void)! = nil, onFailure: ((NSError) -> Void)! = nil) {
 		var sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
 		sessionConfig.HTTPShouldSetCookies = true
 		let req = AFHTTPSessionManager(sessionConfiguration: sessionConfig)
 		req.responseSerializer.acceptableContentTypes = NSSet(object: "text/html")
 		
-		var param = ["mode": mode as AnyObject]
+		var param = ["mode": mode.toRaw() as AnyObject]
 		if topicid != nil {
 			param.updateValue(topicid!, forKey: "topicid")
 		}
@@ -99,23 +110,27 @@ class TessoApiManager: NSObject {
 	}
 
 	func getTimeline(#topicid: Int, maxid: Int? = nil, sinceid: Int? = nil, onSuccess: ((NSDictionary) -> Void)! = nil, onFailure: ((NSError) -> Void)! = nil) {
-		self.getData(mode: 1, topicid: topicid, maxid: maxid, sinceid: sinceid, onSuccess: onSuccess, onFailure: onFailure)
+		self.getData(mode: .Timeline, topicid: topicid, maxid: maxid, sinceid: sinceid, onSuccess: onSuccess, onFailure: onFailure)
 	}
 	
 	func getTopic(onSuccess: ((NSDictionary) -> Void)! = nil, onFailure: ((NSError) -> Void)! = nil) {
-		self.getData(mode: 2, tag: "1", onSuccess: onSuccess, onFailure: onFailure)
+		self.getData(mode: .Topic, tag: "1", onSuccess: onSuccess, onFailure: onFailure)
 	}
     
 	func getClass(onSuccess: ((NSDictionary) -> Void)! = nil, onFailure: ((NSError) -> Void)! = nil) {
-		self.getData(mode: 3, onSuccess: onSuccess, onFailure: onFailure)
+		self.getData(mode: .Class, onSuccess: onSuccess, onFailure: onFailure)
 	}
 	
 	func getProfile(#username: String, onSuccess: ((NSDictionary) -> Void)! = nil, onFailure: ((NSError) -> Void)! = nil) {
-		self.getData(mode: 4, tag: "1", type: 1, username: username, onSuccess: onSuccess, onFailure: onFailure)
+		self.getData(mode: .Profile, tag: "1", type: 1, username: username, onSuccess: onSuccess, onFailure: onFailure)
 	}
 	
-	func getSearchResult(topicid: Int? = 1, maxid: Int? = nil, sinceid: Int? = nil, tag: String? = nil, username: String? = nil, type: Int? = nil, onSuccess: ((NSDictionary) -> Void)! = nil, onFailure: ((NSError) -> Void)! = nil) {
-		self.getData(mode: 5, maxid: maxid, sinceid: sinceid, tag: tag, username: username, type: type, onSuccess: onSuccess, onFailure: onFailure)
+	func getSearchResult(topicid: Int? = 1, maxid: Int? = nil, sinceid: Int? = nil, tag: String? = nil, username: String? = nil, type: TesSoMeSearchType = TesSoMeSearchType.All, onSuccess: ((NSDictionary) -> Void)! = nil, onFailure: ((NSError) -> Void)! = nil) {
+		var typeValue: Int? = type.toRaw()
+		if type == .All {
+			typeValue = nil
+		}
+		self.getData(mode: .SearchResult, maxid: maxid, sinceid: sinceid, tag: tag, username: username, type: typeValue, onSuccess: onSuccess, onFailure: onFailure)
 	}
 	
 }
