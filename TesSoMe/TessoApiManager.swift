61814add1363e8cbffc7f70351a69fadfdaea7ca
Yuki MIZUNO <mizuyuu0904@gmail.com>
2014-09-16 13:02:13 +0900

    Change indent type to tab

--- TesSoMe/TessoApiManager.swift
+++ TesSoMe/TessoApiManager.swift
@@ -9,69 +9,69 @@
 import UIKit
 
 class TessoApiManager: NSObject {
-    let apiEndPoint = "https://tesso.pw/sns/api"
-    
-    func signIn(#userId: String, password: String, onSuccess: (() -> Void)! = nil, onFailure: ((NSError) -> Void)! = nil) {
-        var sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
-        sessionConfig.HTTPShouldSetCookies = true
-        let req = AFHTTPSessionManager(sessionConfiguration: sessionConfig)
-        req.responseSerializer = AFHTTPResponseSerializer()
-        req.POST("https://tesso.pw/users/sign_in", parameters: [ "data[User][username]":userId, "data[User][password]":password], success: {
-            res, data in
-            if res.response!.URL == NSURL(string: "https://tesso.pw/sns") && onSuccess != nil {
-                onSuccess()
-            } else if onFailure != nil {
-                let err = NSError()
-                onFailure(err)
-            }
-            }, failure: {
-                res, err in
-                if onFailure != nil {
-                    onFailure(err)
-                }
-        })
-    }
-    
-    func signOut(onSuccess: (() -> Void)! = nil, onFailure: ((NSError) -> Void)! = nil) {
-        var url = NSURL(string: "https://tesso.pw/users/sign_out")
-        var req = NSMutableURLRequest(URL: url)
-        NSURLConnection.sendAsynchronousRequest(req, queue: NSOperationQueue.mainQueue(), completionHandler: {
-        res, data, err in
-            if err != nil && onFailure != nil {
-                onFailure(err)
-            } else if onSuccess != nil {
-                onSuccess()
-            }
-        })
-    }
-    
-    func getData(#mode: Int, topicid: Int? = nil, maxid: Int? = nil, sinceid: Int? = nil, tag: String? = nil, username: String? = nil, type: String? = nil, onSuccess: ((NSURLSessionDataTask!, AnyObject!) -> Void)! = nil, onFailure: ((NSURLSessionDataTask!, NSError!) -> Void)! = nil) {
-        var sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
-        sessionConfig.HTTPShouldSetCookies = true
-        let req = AFHTTPSessionManager(sessionConfiguration: sessionConfig)
-        req.responseSerializer.acceptableContentTypes = NSSet(object: "text/html")
-        
-        var param = ["mode": mode as AnyObject]
-        if topicid != nil {
-            param.updateValue(topicid!, forKey: "topicid")
-        }
-        if maxid != nil {
-            param.updateValue(maxid!, forKey: "maxid")
-        }
-        if sinceid != nil {
-            param.updateValue(sinceid!, forKey: "sinceid")
-        }
-        if tag != nil {
-            param.updateValue(tag!, forKey: "tag")
-        }
-        if username != nil {
-            param.updateValue(username!, forKey: "username")
-        }
-        if type != nil {
-            param.updateValue(type!, forKey: "type")
-        }
-        req.GET("\(apiEndPoint)/get", parameters: param, success: onSuccess, failure: onFailure)
-    }
-    
-    
+	let apiEndPoint = "https://tesso.pw/sns/api"
+	
+	func signIn(#userId: String, password: String, onSuccess: (() -> Void)! = nil, onFailure: ((NSError) -> Void)! = nil) {
+		var sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
+		sessionConfig.HTTPShouldSetCookies = true
+		let req = AFHTTPSessionManager(sessionConfiguration: sessionConfig)
+		req.responseSerializer = AFHTTPResponseSerializer()
+		req.POST("https://tesso.pw/users/sign_in", parameters: [ "data[User][username]":userId, "data[User][password]":password], success: {
+			res, data in
+			if res.response!.URL == NSURL(string: "https://tesso.pw/sns") && onSuccess != nil {
+				onSuccess()
+			} else if onFailure != nil {
+				let err = NSError()
+				onFailure(err)
+			}
+			}, failure: {
+				res, err in
+				if onFailure != nil {
+					onFailure(err)
+				}
+		})
+	}
+	
+	func signOut(onSuccess: (() -> Void)! = nil, onFailure: ((NSError) -> Void)! = nil) {
+		var url = NSURL(string: "https://tesso.pw/users/sign_out")
+		var req = NSMutableURLRequest(URL: url)
+		NSURLConnection.sendAsynchronousRequest(req, queue: NSOperationQueue.mainQueue(), completionHandler: {
+		res, data, err in
+			if err != nil && onFailure != nil {
+				onFailure(err)
+			} else if onSuccess != nil {
+				onSuccess()
+			}
+		})
+	}
+	
+	func getData(#mode: Int, topicid: Int? = nil, maxid: Int? = nil, sinceid: Int? = nil, tag: String? = nil, username: String? = nil, type: String? = nil, onSuccess: ((NSURLSessionDataTask!, AnyObject!) -> Void)! = nil, onFailure: ((NSURLSessionDataTask!, NSError!) -> Void)! = nil) {
+		var sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
+		sessionConfig.HTTPShouldSetCookies = true
+		let req = AFHTTPSessionManager(sessionConfiguration: sessionConfig)
+		req.responseSerializer.acceptableContentTypes = NSSet(object: "text/html")
+		
+		var param = ["mode": mode as AnyObject]
+		if topicid != nil {
+			param.updateValue(topicid!, forKey: "topicid")
+		}
+		if maxid != nil {
+			param.updateValue(maxid!, forKey: "maxid")
+		}
+		if sinceid != nil {
+			param.updateValue(sinceid!, forKey: "sinceid")
+		}
+		if tag != nil {
+			param.updateValue(tag!, forKey: "tag")
+		}
+		if username != nil {
+			param.updateValue(username!, forKey: "username")
+		}
+		if type != nil {
+			param.updateValue(type!, forKey: "type")
+		}
+		req.GET("\(apiEndPoint)/get", parameters: param, success: onSuccess, failure: onFailure)
+	}
+	
+	
 }
--- TesSoMeTests/TesSoMeTests.swift
+++ TesSoMeTests/TesSoMeTests.swift
@@ -10,59 +10,59 @@ import UIKit
 import XCTest
 
 class TesSoMeTests: XCTestCase {
-    let userId = "YOUR_USERID_HERE"
-    let password = "YOUR_PASSWORD_HERE"
-    
-    let apiMgr = TessoApiManager()
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        let apiExpectation = self.expectationWithDescription("api signin")
-        
-        apiMgr.signIn(userId: self.userId, password: self.password, onSuccess: {
-            XCTAssert(true, "Pass")
-            apiExpectation.fulfill()
-            }, onFailure: { err in
-                XCTAssert(false, "Fail")
-        })
-        
-        self.waitForExpectationsWithTimeout(1.0, handler: {err in
-        })
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        apiMgr.signOut(onSuccess: nil, onFailure: nil)
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testGetTimeline() {
-        let apiExpectation = self.expectationWithDescription("api signin")
-        
-        apiMgr.getData(mode: 1, topicid: 1, onSuccess:
-            {res, data in
-                XCTAssert(true, "Pass")
-                apiExpectation.fulfill()
-            }
-            , onFailure: {res, err in
-                XCTAssert(false, "Fail")
-        })
-        self.waitForExpectationsWithTimeout(3.0, handler: {err in
-        })
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-            //XCTAssertEqual("a", "b", "err")
-        }
-    }
-    
+	let userId = "YOUR_USERID_HERE"
+	let password = "YOUR_PASSWORD_HERE"
+	
+	let apiMgr = TessoApiManager()
+	
+	override func setUp() {
+		super.setUp()
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+		let apiExpectation = self.expectationWithDescription("api signin")
+		
+		apiMgr.signIn(userId: self.userId, password: self.password, onSuccess: {
+			XCTAssert(true, "Pass")
+			apiExpectation.fulfill()
+			}, onFailure: { err in
+				XCTAssert(false, "Fail")
+		})
+		
+		self.waitForExpectationsWithTimeout(1.0, handler: {err in
+		})
+	}
+	
+	override func tearDown() {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+		apiMgr.signOut(onSuccess: nil, onFailure: nil)
+		super.tearDown()
+	}
+	
+	func testExample() {
+		// This is an example of a functional test case.
+		XCTAssert(true, "Pass")
+	}
+	
+	func testGetTimeline() {
+		let apiExpectation = self.expectationWithDescription("api signin")
+		
+		apiMgr.getData(mode: 1, topicid: 1, onSuccess:
+			{res, data in
+				XCTAssert(true, "Pass")
+				apiExpectation.fulfill()
+			}
+			, onFailure: {res, err in
+				XCTAssert(false, "Fail")
+		})
+		self.waitForExpectationsWithTimeout(3.0, handler: {err in
+		})
+	}
+	
+	func testPerformanceExample() {
+		// This is an example of a performance test case.
+		self.measureBlock() {
+			// Put the code you want to measure the time of here.
+			//XCTAssertEqual("a", "b", "err")
+		}
+	}
+	
 }
