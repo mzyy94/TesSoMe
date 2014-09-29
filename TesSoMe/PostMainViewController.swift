5219e5bdb498228b0797f668d5192ec73fac28ec
Yuki MIZUNO <mizuyuu0904@gmail.com>
2014-09-29 13:22:16 +0900

    Add file uploading function

--- TesSoMe/PostMainViewController.swift
+++ TesSoMe/PostMainViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class PostMainViewController: UIViewController {
+class PostMainViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 	let app = UIApplication.sharedApplication()
 	let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
 	let ud = NSUserDefaults()
 
 	var menu: REMenu!
+	var messageType: TessoMessageType = .Unknown
 	var preparedText = ""
+	
+    var fileURLtoPost: NSURL? = nil
 	
 	@IBOutlet weak var postTitleBtn: UIButton!
 	@IBOutlet weak var textView: UITextView!
@@ -38,6 +41,7 @@ class PostMainViewController: UIViewController {
 		self.textView.text = preparedText
 		
 		setTitleBtnText("Message")
+		messageType = .Message
 		
 		self.providesPresentationContextTransitionStyle = true
 		self.definesPresentationContext = true
@@ -53,15 +57,35 @@ class PostMainViewController: UIViewController {
 		let selectMessageItem = REMenuItem(title: NSLocalizedString("Message", comment: "Message on navigation bar"), image: nil, highlightedImage: nil, action: {
 			item in
 			self.setTitleBtnText("Message")
+			self.messageType = .Message
 			self.showKeyboard()
 		})
 		let selectFileUploadItem = REMenuItem(title: NSLocalizedString("File upload", comment: "File upload on navigation bar"), image: nil, highlightedImage: nil, action: {
 			item in
-			self.setTitleBtnText("File upload")
+			let oldMenuItems = self.menu.items
+			func generateChoosePhotoFunc(own: PostMainViewController, #doAfter: () -> Void)(type: UIImagePickerControllerSourceType)(item: REMenuItem!) {
+                let picker = UIImagePickerController()
+                picker.delegate = own
+				picker.sourceType = type
+				picker.mediaTypes = UIImagePickerController.availableMediaTypesForSourceType(type)!
+				own.presentViewController(picker, animated: true, completion: nil)
+				doAfter()
+			}
+
+			let choosePhoto = generateChoosePhotoFunc(self, doAfter: {self.menu.items = oldMenuItems})
+			
+			let takePhoto = REMenuItem(title: NSLocalizedString("Camera", comment: "Camera"), image: nil, highlightedImage: nil, action: choosePhoto(type: .Camera))
+			
+			let selectPhoto = REMenuItem(title: NSLocalizedString("Photo Library", comment: "Photo Library"), image: nil, highlightedImage: nil, action: choosePhoto(type: .PhotoLibrary))
+			
+			self.menu.items = [takePhoto, selectPhoto]
+			self.menu.showFromNavigationController(self.navigationController)
+			
 		})
 		let selectDrawingItem = REMenuItem(title: NSLocalizedString("Drawing", comment: "Drawing on navigation bar"), image: nil, highlightedImage: nil, action: {
 			item in
 			self.setTitleBtnText("Drawing")
+			self.messageType = .Drawing
 		})
 		self.menu = REMenu(items: [selectMessageItem, selectFileUploadItem, selectDrawingItem])
 		menu.liveBlur = true
@@ -69,6 +93,34 @@ class PostMainViewController: UIViewController {
 		menu.separatorColor = UIColor(white: 0.0, alpha: 0.4)
 		menu.borderColor = UIColor.clearColor()
 		menu.textColor = UIColor(white: 1.0, alpha: 0.78)
+	}
+	
+	func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+				let infoDic = NSDictionary(dictionary: info)
+		let mediaType = infoDic[UIImagePickerControllerMediaType] as String
+		if mediaType == "public.image" {
+			let image = infoDic[UIImagePickerControllerOriginalImage] as UIImage
+            let fileName = "Image.jpg"
+            var tmpDir = NSURL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+            let fileToPost = UIImageJPEGRepresentation(image, 0.8)
+            let fileManager = NSFileManager.defaultManager()
+            let now = NSDate()
+            var err: NSError? = nil
+            tmpDir = tmpDir.URLByAppendingPathComponent("\(now.timeIntervalSince1970)")
+            fileManager.createDirectoryAtURL(tmpDir, withIntermediateDirectories: true, attributes: nil, error: &err)
+            println(err?.localizedDescription)
+            if err == nil {
+                fileURLtoPost = tmpDir.URLByAppendingPathComponent(fileName)
+                fileManager.createFileAtPath(fileURLtoPost!.relativePath!, contents: fileToPost, attributes: nil)
+				self.setTitleBtnText("File upload")
+				self.messageType = .File
+			}
+		} else {
+            fileURLtoPost = NSURL(string: infoDic[UIImagePickerControllerMediaURL] as String)
+			self.setTitleBtnText("File upload")
+			self.messageType = .File
+        }
+		picker.dismissViewControllerAnimated(true, completion: nil)
 	}
 	
 	func setTitleBtnText(text: String) {
@@ -103,23 +155,39 @@ class PostMainViewController: UIViewController {
 	func sendPost() {
 		let apiManager = TessoApiManager()
 		let text = self.textView.text
-		closeView({
-			apiManager.sendMessage(topicid: 1, message: text, onSuccess: nil, onFailure: {
-				err in
-				let notification = MPGNotification(title: NSLocalizedString("Post failed.", comment: "Post failed."), subtitle: err.localizedDescription, backgroundColor: UIColor(red: 1.0, green: 0.3, blue: 0.3, alpha: 1.0), iconImage: UIImage(named: "warning_icon"))
-				notification.duration = 5.0
-				notification.animationType = .Drop
-				notification.setButtonConfiguration(.OneButton, withButtonTitles: [NSLocalizedString("Edit", comment: "Edit")])
-				notification.buttonHandler = {
-					notification, buttonIndex in
-					if buttonIndex == notification.firstButton.tag ||
-						buttonIndex == notification.backgroundView.tag {
-							self.app.openURL(NSURL(string: NSString(format: "tesso://post/?text=%@", text)))
-					}
+		
+		func failureAction(err: NSError) {
+			let notification = MPGNotification(title: NSLocalizedString("Post failed.", comment: "Post failed."), subtitle: err.localizedDescription, backgroundColor: UIColor(red: 1.0, green: 0.3, blue: 0.3, alpha: 1.0), iconImage: UIImage(named: "warning_icon"))
+			notification.duration = 5.0
+			notification.animationType = .Drop
+			notification.setButtonConfiguration(.OneButton, withButtonTitles: [NSLocalizedString("Edit", comment: "Edit")])
+			notification.buttonHandler = {
+				notification, buttonIndex in
+				if buttonIndex == notification.firstButton.tag ||
+					buttonIndex == notification.backgroundView.tag {
+						self.app.openURL(NSURL(string: NSString(format: "tesso://post/?text=%@", text)))
 				}
-				notification.show()
+			}
+			notification.show()
+		}
+		let onFailure = failureAction
+		
+		switch messageType {
+		case .Message:
+			closeView({
+				apiManager.sendMessage(topicid: 1, message: text, onSuccess: nil, onFailure: onFailure)
 			})
-		})
+		case .File:
+			closeView({
+				apiManager.uploadFile(fileURL: self.fileURLtoPost!, onSuccess: nil, onFailure: onFailure)(topicid: 1)
+			})
+		case .Drawing:
+			closeView({
+				apiManager.sendMessage(topicid: 1, message: text, onSuccess: nil, onFailure: onFailure)
+			})
+		default:
+			NSLog("Unknown message type to post (value: %d)", messageType.toRaw())
+		}
 	}
 	
     override func didReceiveMemoryWarning() {
