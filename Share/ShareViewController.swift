a6e3ca27aadd312ba8ed3eca7e48b471a9694b25
Yuki MIZUNO <mizuyuu0904@gmail.com>
2014-10-14 19:22:10 +0900

    Add file share extension

--- Share/ShareViewController.swift
+++ Share/ShareViewController.swift
@@ -12,15 +12,17 @@ import Social
 class ShareViewController: SLComposeServiceViewController, UITableViewDelegate, UITableViewDataSource {
 	
 	let tesso_maxCharactersLimit = 1024
+	let tesso_maxFilenameLimit = 64
 	let apiManager = TessoApiManager()
 
 	var topicId = 1
 	var topics: [NSDictionary] = []
+	
+	var isFileShare = true
 
 	let selectTopicViewController = UITableViewController()
 	
 	override func viewDidLoad() {
-		self.isContentValid()
 		self.navigationController?.navigationBar.backgroundColor = UIColor.globalTintColor(alpha: 0.3)
 		self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
 		self.title = "TesSoMe"
@@ -31,6 +33,25 @@ class ShareViewController: SLComposeServiceViewController, UITableViewDelegate, 
 		selectTopicViewController.tableView.delegate = self
 		selectTopicViewController.tableView.dataSource = self
 
+		let item = self.extensionContext!.inputItems[0] as NSExtensionItem
+		let itemProvider = item.attachments![0] as NSItemProvider
+		
+		if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeImage as NSString) {
+			self.textView.text = "Image.jpg"
+			self.isContentValid()
+		} else if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeFileURL as NSString) {
+			itemProvider.loadItemForTypeIdentifier(kUTTypeFileURL as NSString, options: nil, completionHandler: { (item, error) in
+				let fileName = (item as NSURL).lastPathComponent
+				dispatch_sync(dispatch_get_main_queue(), {
+					self.textView.text = fileName
+					self.isContentValid()
+				})
+			})
+		} else if itemProvider.hasItemConformingToTypeIdentifier("public.url") {
+			isFileShare = false
+			self.isContentValid()
+		}
+		
 		let serviceName = "TesSoMe"
 		let accounts = SSKeychain.accountsForService(serviceName)
 		if accounts != nil {
@@ -62,19 +83,86 @@ class ShareViewController: SLComposeServiceViewController, UITableViewDelegate, 
 	override func isContentValid() -> Bool {
 		if let currentMessage = contentText {
 			let currentMessageLength = countElements(currentMessage)
-			self.charactersRemaining = self.tesso_maxCharactersLimit - currentMessageLength
+			if isFileShare {
+				self.charactersRemaining = self.tesso_maxFilenameLimit - currentMessageLength
+			} else {
+				self.charactersRemaining = self.tesso_maxCharactersLimit - currentMessageLength
+			}
 			if Int(currentMessageLength) == 0 || Int(self.charactersRemaining) < 0 {
 				return false
 			}
 		}
 		return true
 	}
+	
+	func saveDataToTmp(data: NSData, fileName: String) -> NSURL! {
+		let fileManager = NSFileManager.defaultManager()
+		var tmpDir = NSURL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+		let now = NSDate()
+		var err: NSError? = nil
+		tmpDir = tmpDir.URLByAppendingPathComponent("\(now.timeIntervalSince1970)")
+		fileManager.createDirectoryAtURL(tmpDir, withIntermediateDirectories: true, attributes: nil, error: &err)
+		if err == nil {
+			let fileURLtoPost = tmpDir.URLByAppendingPathComponent(fileName)
+			fileManager.createFileAtPath(fileURLtoPost.relativePath!, contents: data, attributes: nil)
+			return fileURLtoPost
+		}
+		return nil
+	}
 
 	override func didSelectPost() {
 		let item = self.extensionContext!.inputItems[0] as NSExtensionItem
 		let itemProvider = item.attachments![0] as NSItemProvider
 		
-		if itemProvider.hasItemConformingToTypeIdentifier("public.url") {
+		if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeFileURL as NSString) {
+			itemProvider.loadItemForTypeIdentifier(kUTTypeFileURL as NSString, options: nil, completionHandler: { (item, error) in
+				let fileURL = item as NSURL
+				let data = NSData(contentsOfURL: fileURL)
+				let fileName = self.contentText
+				
+				if let fileURL = self.saveDataToTmp(data, fileName: fileName) {
+					self.apiManager.uploadFile(fileURL: fileURL, onSuccess:
+						{ data in
+							NSLog("Share Extension: Success (File share)")
+							self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+						}
+						, onFailure:
+						{ err in
+							NSLog("Share Extension: Post Failed (File share), %@", err.localizedDescription)
+							self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+						}
+						)(topicid: self.topicId)
+				} else {
+					NSLog("Share Extension: Post Failed (File share), Can't save file")
+					self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+				}
+			})
+		} else if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeImage as NSString) {
+			itemProvider.loadItemForTypeIdentifier(kUTTypeImage as NSString, options: nil, completionHandler: { (item, error) in
+				let imageURL = item as NSURL
+				let imgData = NSData(contentsOfURL: imageURL)
+				let image = UIImage(data: imgData)
+				let data = UIImageJPEGRepresentation(image, 0.8)
+				let fileName = self.contentText
+				
+				if let fileURL = self.saveDataToTmp(data, fileName: fileName) {
+					self.apiManager.uploadFile(fileURL: fileURL, onSuccess:
+						{ data in
+							NSLog("Share Extension: Success (Image share)")
+							self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+						}
+						, onFailure:
+						{ err in
+							NSLog("Share Extension: Post Failed (Image share), %@", err.localizedDescription)
+							self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+						}
+						)(topicid: self.topicId)
+				} else {
+					NSLog("Share Extension: Post Failed (Image share), Can't save file")
+					self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+				}
+			})
+		} else if itemProvider.hasItemConformingToTypeIdentifier("public.url") {
 			itemProvider.loadItemForTypeIdentifier("public.url", options: nil, completionHandler: { (item, error) in
 				let url = item as NSURL
 				var urlString = url.absoluteString?.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
