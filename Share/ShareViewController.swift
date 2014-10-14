c88741094137c153849e3ed67aa196af0b2e2757
Yuki MIZUNO <mizuyuu0904@gmail.com>
2014-10-14 16:23:14 +0900

    Add URL-Share

--- Share/ShareViewController.swift
+++ Share/ShareViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 import Social
 
-class ShareViewController: SLComposeServiceViewController {
+class ShareViewController: SLComposeServiceViewController, UITableViewDelegate, UITableViewDataSource {
 	
 	let tesso_maxCharactersLimit = 1024
-	
+	let apiManager = TessoApiManager()
+
+	var topicId = 1
+	var topics: [NSDictionary] = []
+
+	let selectTopicViewController = UITableViewController()
 	
 	override func viewDidLoad() {
 		self.isContentValid()
@@ -22,15 +27,43 @@ class ShareViewController: SLComposeServiceViewController {
 		self.textView.tintColor = UIColor.globalTintColor()
 
 		self.navigationController?.navigationBar.backgroundColor = UIColor.globalTintColor()
+		
+		selectTopicViewController.tableView.delegate = self
+		selectTopicViewController.tableView.dataSource = self
 
+		let serviceName = "TesSoMe"
+		let accounts = SSKeychain.accountsForService(serviceName)
+		if accounts != nil {
+			let account = accounts.last as? NSDictionary
+			let username = account!["acct"] as String!
+			let password = SSKeychain.passwordForService(serviceName, account: username)
+			self.apiManager.signIn(username: username, password: password, onSuccess:
+				{
+					self.apiManager.getTopic(onSuccess:
+						{ data in
+							self.topics = data["data"] as [NSDictionary]
+							self.selectTopicViewController.tableView.reloadData()
+							self.topicConfig.valuePending = false
+						}
+						, onFailure:
+						{ err in
+							self.topicConfig.valuePending = false
+						}
+					)
+				}
+				, onFailure:
+				{ err in
+					
+				}
+			)
+		}
 	}
 	
 	override func isContentValid() -> Bool {
-		// Do validation of contentText and/or NSExtensionContext attachments here
 		if let currentMessage = contentText {
 			let currentMessageLength = countElements(currentMessage)
 			self.charactersRemaining = self.tesso_maxCharactersLimit - currentMessageLength
-			if Int(self.charactersRemaining) < 0 {
+			if Int(currentMessageLength) == 0 || Int(self.charactersRemaining) < 0 {
 				return false
 			}
 		}
@@ -38,48 +71,90 @@ class ShareViewController: SLComposeServiceViewController {
 	}
 
 	override func didSelectPost() {
-		// This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-	
-		// Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-		self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+		let item = self.extensionContext!.inputItems[0] as NSExtensionItem
+		let itemProvider = item.attachments![0] as NSItemProvider
+		
+		if itemProvider.hasItemConformingToTypeIdentifier("public.url") {
+			itemProvider.loadItemForTypeIdentifier("public.url", options: nil, completionHandler: { (item, error) in
+				let url = item as NSURL
+				var urlString = url.absoluteString?.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+				
+				self.apiManager.sendMessage(topicid: self.topicId, message: "\(self.contentText) \n\(urlString)", onSuccess:
+					{ data in
+						NSLog("Share Extension: Success (URL share)")
+						self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+					}
+					, onFailure:
+					{ err in
+						NSLog("Share Extension: Post Failed (URL share), %@", err.localizedDescription)
+						self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+					}
+				)
+
+				
+			})
+		} else {
+			apiManager.sendMessage(topicid: topicId, message: self.contentText, onSuccess:
+				{ data in
+					NSLog("Share Extension: Success")
+					self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+				}
+				, onFailure:
+				{ err in
+					NSLog("Share Extension: Post Failed, %@", err.localizedDescription)
+					self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+				}
+			)
+		}
+		
 	}
 
+	let topicConfig = SLComposeSheetConfigurationItem()
+	
 	override func configurationItems() -> [AnyObject]! {
 		var configurationItems: [SLComposeSheetConfigurationItem] = []
 		
-		if self.extensionContext?.inputItems.count == 0 {
-			let attachmentConfig = SLComposeSheetConfigurationItem()
-			attachmentConfig.title = NSLocalizedString("File attachment", comment: "File attachment")
-			attachmentConfig.value = NSLocalizedString("None", comment: "None")
-			attachmentConfig.tapHandler = {
-				let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
+		topicConfig.title = NSLocalizedString("Topic", comment: "Topic")
+		topicConfig.value = "\(topicId + 99)"
+		topicConfig.valuePending = true
 
-				let takePhoto = UIAlertAction(title: NSLocalizedString("Camera", comment: "Camera"), style: .Default, handler: nil)
-				actionSheet.addAction(takePhoto)
-
-				let choosePhoto = UIAlertAction(title: NSLocalizedString("Photo Library", comment: "Photo Library"), style: .Default, handler: nil)
-				actionSheet.addAction(choosePhoto)
-				
-				let cancelBtn = UIAlertAction(title: NSLocalizedString("Cancel", comment: "Cancel"), style: .Cancel, handler: nil)
-				actionSheet.addAction(cancelBtn)
-
-				self.presentViewController(actionSheet, animated: true, completion: nil)
-
-			}
-			configurationItems.append(attachmentConfig)
-			
-			let drawingConfig = SLComposeSheetConfigurationItem()
-			drawingConfig.title = NSLocalizedString("Drawing", comment: "Drawing")
-			drawingConfig.value = NSLocalizedString("None", comment: "None")
-			drawingConfig.tapHandler = {
-				let view = UIViewController()
-				self.pushConfigurationViewController(view)
-			}
-			configurationItems.append(drawingConfig)
+		topicConfig.tapHandler = {
+			self.pushConfigurationViewController(self.selectTopicViewController)
 		}
 		
-		// To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+		configurationItems.append(topicConfig)
+
 		return configurationItems
 	}
 
+	func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return topics.count
+	}
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = UITableViewCell()
+		if indexPath.row >= topics.count {
+			return cell
+		}
+		let topic = topics[indexPath.row]
+		let topicNumber = (topic["id"] as String!).toInt()! + 99
+		let titleAttributedText = NSMutableAttributedString(string: "\(topicNumber) ", attributes: [NSForegroundColorAttributeName: UIColor.globalTintColor()])
+		titleAttributedText.appendAttributedString(NSAttributedString(string: topic["data"] as String!))
+		cell.textLabel?.attributedText = titleAttributedText
+		return cell
+	}
+	
+	func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+		popConfigurationViewController()
+		
+		let topic = topics[indexPath.row]
+		self.topicId = (topic["id"] as String!).toInt()!
+		self.topicConfig.value = "\(topicId + 99)"
+		
+		return indexPath
+	}
 }
